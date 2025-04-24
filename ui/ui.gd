e417a87187bc34544faa4060959236363f38b494
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com.>
2025-04-24 22:03:32 +0200

    Merge local changes.

--- ui/ui.gd
+++ ui/ui.gd
@@ -2,10 +2,12 @@ class_name UI
 extends Control
 
 
+@export var inventory_manager: InventoryManager
 
 func _gui_input(p_event: InputEvent) -> void:
 	if p_event is InputEventMouseButton:
-		if p_event.button_index == MOUSE_BUTTON_LEFT and p_event.is_released():
-			if $InventoryManager/DragSlot.visible:
+		var mouse_event := p_event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.is_released():
+			if inventory_manager._is_selected() and inventory_manager.selected_inventory_view.inventory.editable:
 				print("DELETE ITEM")
 			
